1a369b721760967544c65e97fb49d66448673950
Bill Wilke <billwilke2@gmail.com>
2020-06-03 16:39:17 -0600

    Beginning index.js and onload function

--- src/index.js
+++ src/index.js
@@ -7,4 +7,47 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+import DataRepository from './DataRepository';
+import DomUpdates from './domUpdates'
+
+let travelers;
+let destinations;
+let trips;
+const domUpdates = new DomUpdates()
+
+//Fetching
+travelers = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers')
+  .then(data => data.json())
+  .then(data => data.travelers)
+  .catch(err => console.log(err.message))
+
+destinations = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/destinations/destinations')
+  .then(data => data.json())
+  .then(data => data.destinations)
+  .catch(err => console.log(err.message))
+
+trips = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/trips/trips')
+  .then(data => data.json())
+  .then(data => data.recipeData)
+  .catch(err => console.log(err.message))
+
+
+//PROMISE
+Promise.all([travelers, destinations, trips])
+  .then(data => {
+    travelers = data[0];
+    destinations = data[1]
+    trips = data[2]
+  })
+  .then(() => {
+    onStartUp()
+  })
+  .catch(error => {
+    console.log('Something is amiss with promise all', error)
+  });
+
+//Functions
+function onStartup() {
+
+}
+
