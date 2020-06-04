6f6f533200954ed871a7b9bb0b02a3d24ef87119
Bill Wilke <billwilke2@gmail.com>
2020-06-04 16:39:24 -0600

    Adjusts to have User parent class, adds an accumulate totalmethod, and test suite for User

--- test/TravelAgency-test.js
+++ test/TravelAgency-test.js
@@ -7,11 +7,14 @@ import TravelAgency from '../src/TravelAgency.js'
 import travelersData from './data/travelers.js'
 import destinationData from './data/destinations.js'
 import tripsData from './data/trips.js'
+import User from '../src/User.js'
 
 let travelAgency;
+let user;
 
 describe('Travel Agency', function() {
     beforeEach(() => {
+        user = new User(travelersData, destinationData, tripsData)
         travelAgency = new TravelAgency()
     })
 
@@ -50,6 +53,7 @@ describe('Travel Agency', function() {
     describe('Travel Agency Methods', function() {
         beforeEach(() => {
             let currentDate = "2020/06/29"
+            user = new User(travelersData, destinationData, tripsData)
             travelAgency = new TravelAgency(travelersData, destinationData, tripsData, currentDate)
         })
         it('should be able to see all requested Trips', function() {
@@ -94,11 +98,15 @@ describe('Travel Agency', function() {
         })
 
         it('should get the total expenditure of all customers', function() {
-            expect(travelAgency.totalExpenditureWithoutProfit()).to.equal(434275)
+            expect(travelAgency.totalExpenditureWithoutProfit(tripsData)).to.equal(43325)
         })
 
         it('should be able to calculate total Income', function() {
-            expect(travelAgency.allIncome()).to.equal(43427.5)
+            expect(travelAgency.agencyMargin(tripsData)).to.equal(4332.5)
+        })
+
+        it('should be able to bring back the total people have spent at the agency' , function() {
+            expect(travelAgency.accumulatedTotal(tripsData)).to.equal(47657.5)
         })
 
     })
---
+++ test/Traveler-test.js
@@ -0,0 +1,88 @@
+const chai = require('chai');
+const expect = chai.expect;
+const spies = require("chai-spies");
+chai.use(spies);
+
+import TravelAgency from '../src/TravelAgency.js'
+import travelersData from './data/travelers.js'
+import destinationData from './data/destinations.js'
+import tripsData from './data/trips.js'
+import Traveler from '../src/Traveler.js'
+
+let traveler;
+
+describe('Traveler', function() {
+    beforeEach(() => {
+        traveler = new Traveler()
+    })
+
+    it('should be a function', function() {
+        expect(Traveler).to.be.a('function');
+    });
+    
+    it('should be an instance of a Traveler', function () {
+        expect(traveler).to.be.an.instanceof(Traveler);
+    });
+    
+    it('should not require an argument to create a new Traveler', () => {
+        expect(() => {
+          new Traveler() 
+        }).to.not.throw(Error);
+    });
+
+    describe('Traveler Data', function() {
+        beforeEach(() => {
+            traveler = new Traveler(travelersData, destinationData, tripsData)
+        })
+        
+        it('should hold onto travelers data', function() {
+            expect(traveler.travelersData).to.equal(travelersData)
+        })
+
+        it('should hold onto destination data', function() {
+            expect(traveler.destinationData).to.equal(destinationData)
+        })
+
+        it('should hold onto trips data', function() {
+            expect(traveler.tripsData).to.equal(tripsData)
+        })
+    })
+
+    describe('Traveler Instance', function() {
+        this.beforeEach(() => {
+            traveler = new Traveler(travelersData, destinationData, tripsData, travelersData[0])
+        })
+
+        it('should have a traveler id', function() {
+            expect(traveler.id).to.equal(1)
+        })
+
+        it('should have a name', function() {
+            expect(traveler.name).to.equal("Ham Leadbeater")
+        })
+
+        it('should have a traveler type', function() {
+            expect(traveler.travelerType).to.equal("relaxer")
+        })
+        
+        it('should have an array of all their trips', function() {
+            expect(traveler.trips).to.deep.equal([
+                {
+                    "id": 8,
+                    "userID": 1,
+                    "destinationID": 8,
+                    "travelers": 6,
+                    "date": "2021/02/07",
+                    "duration": 4,
+                    "status": "approved",
+                    "suggestedActivities": []
+                }
+            ])
+        })
+        
+        it('should calculate all of its accumulated trips costs', function() {
+            expect(traveler.accumulatedTotal(traveler.trips)).to.equal(7150)
+        });
+
+    })
+});
