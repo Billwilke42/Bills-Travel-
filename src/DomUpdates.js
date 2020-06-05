816d5ed40d06284cb46fdb292253f552b8ca1e77
Bill Wilke <billwilke2@gmail.com>
2020-06-05 12:23:49 -0600

    Completes log in functionality and basic display

--- src/DomUpdates.js
+++ src/DomUpdates.js
@@ -1,29 +1,120 @@
+const usernameInput = document.getElementById('username')
+const passwordInput = document.getElementById('password')
+const mainArea = document.querySelector('.main')
 
-class DomUpdates {
-    constructor() {
-        this.counter = 0;
-    }
+const domUpdates = {
 
-    cycleImages(destinations) {
-        this.counter++
-        if(this.counter === destinations.length + 1) {
-            this.counter = 0
+    logIn() {
+        debugger
+        const usernameArray = usernameInput.value.split('')
+        const usernameID = usernameArray.splice(8, 10).join('')
+        if(usernameInput.value === 'agency' && passwordInput.value === 'travel2020') {
+          domUpdates.displayAgencyDashboard()
+        } else if (usernameID <= 50 && passwordInput.value === 'travel2020') {
+          domUpdates.displayTravelerDashboard()
+        } else {
+          domUpdates.displayError()
         }
-        console.log(destinations[this.counter])
-        let mainArea = document.querySelector('.main')
+        event.preventDefault()
+    },
+
+    displayAgencyDashboard() {
+        domUpdates.agencyDashboard()
+        usernameInput.value = ''
+        passwordInput.value = ''
+    },
+
+    agencyDashboard() {
+        mainArea.innerHTML = `<article class='agency-dashboard'>
+        <section class='requested-trips'></section>
+        <section class='travelers-on-trips'></section>
+        <section class='search-for-user'></section>
+        <section class='income'></section>
+        </article>`
+        domUpdates.requestedTrips()
+        domUpdates.travelersOnTrips()
+        domUpdates.searchForUser()
+        domUpdates.income()
+    },
+
+    displayTravelerDashboard() {
+        domUpdates.travelerDashboard()
+        usernameInput.value = ''
+        passwordInput.value = ''
+    },
+
+    requestedTrips() {
+        let tripsRequestedForm = document.querySelector('.requested-trips')
+        tripsRequestedForm.innerHTML = `<h1>Trips Requested:</h1>`
+    },
+
+    travelersOnTrips() {
+        let travelersOnTripsForm = document.querySelector('.travelers-on-trips')
+        travelersOnTripsForm.innerHTML = `<h1>Travelers on trips:</h1>`
+    },
+
+    searchForUser() {
+        let searchForUserForm = document.querySelector('.search-for-user')
+        searchForUserForm.innerHTML = `<h1>Search for Traveler:</h1>`
+    },
+
+    income() {
+        let incomeForm = document.querySelector('.income')
+        incomeForm.innerHTML = `<h1>Total Income:</h1>`
+    },
+
+    travelerDashboard() {
+        mainArea.innerHTML = `<article class='traveler-dashboard'>
+        <section class='all-trips'></section>
+        <section class='amount-spent'></section>
+        <section class='trip-request'></section>
+        <section class='pending-trips'></section>
+        </article>`
+        domUpdates.allTrips()
+        domUpdates.amountSpent()
+        domUpdates.requestTrip()
+        domUpdates.pendingTrips()
+    },
+
+    allTrips() {
+        let allTripsForm = document.querySelector('.all-trips')
+        allTripsForm.innerHTML = `<h1>All Trips:</h1>`
+    },
+
+    amountSpent() {
+        let amountSpentForm = document.querySelector('.amount-spent')
+        amountSpentForm.innerHTML = `<h1>Amount Spent:</h1>`
+    },
+
+    requestTrip() {
+        let requestTripForm = document.querySelector('.trip-request')
+        requestTripForm.innerHTML = `<h1>Request a Trip:</h1>`
+    },
+
+    pendingTrips() {
+        let pendingTripsForm = document.querySelector('.pending-trips')
+        pendingTripsForm.innerHTML = `<h1>Pending Trips:`
+    },
+
+    displayError() {
+        debugger
+        let form = document.getElementById('form1')
+        form.insertAdjacentHTML('afterend', `ERROR`)
+        usernameInput.value = ''
+        passwordInput.value = ''
+    },
+
+    cycleImages(destinations, counter) {
+        counter++
+        if(counter === destinations.length + 1) {
+            counter = 0
+        }
         mainArea.innerHTML = `<header class='welcome-message'><h2>Welcome to Travel Tracker</h2></header>
-        <section class='cycling-images'><img src="${destinations[this.counter].image}" alt="destination-image" class='cycling-images'>
+        <section class='cycling-images'><img src="${destinations[counter].image}" alt="destination-image" class='cycling-images'>
         <footer>Your Vacation Awaits!</footer></section>`
-        setTimeout(this.cycleImages, 1000, destinations);
-    }
+        setTimeout(domUpdates.cycleImages, 3000, destinations, counter);
+      }
 
-    displayUserPage() {
-
-    }
-
-    displayAgentPage() {
-
-    }
 }
 
-export default DomUpdates;
+export default domUpdates;
--- src/index.js
+++ src/index.js
@@ -7,14 +7,15 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-// import DomUpdates from './domUpdates'
+import domUpdates from './domUpdates'
 import User from './User';
+import TravelAgency from './TravelAgency'
+import Traveler from './Traveler'
 
 let travelers;
 let destinations;
 let trips;
 let user;
-// const domUpdates = new DomUpdates()
 
 //Fetching
 travelers = fetch('https://fe-apps.herokuapp.com/api/v1/travel-tracker/data/travelers/travelers')
@@ -42,7 +43,7 @@ Promise.all([travelers, destinations, trips])
   })
   .then(() => {
     user = new User(travelers, destinations, trips)
-    onStartUp()
+    onStartUp(destinations)
   })
   .catch(error => {
     console.log('Something is amiss with promise all', error)
@@ -52,61 +53,15 @@ Promise.all([travelers, destinations, trips])
   let logInButton = document.querySelector('.login-button')
   const usernameInput = document.getElementById('username')
   const passwordInput = document.getElementById('password')
+  const mainArea = document.querySelector('.main')
   
 //Event Listeners 
-logInButton.addEventListener('click', logIn)
+logInButton.addEventListener('click', domUpdates.logIn)
+
 //Functions
-function onStartUp() {
+function onStartUp(destinations) {
     let counter = 0;
-    // cycleImages(destinations, counter)
+    // domUpdates.cycleImages(destinations, counter)
 }
 
-function cycleImages(destinations, counter) {
-    counter++
-    if(counter === destinations.length + 1) {
-        counter = 0
-    }
-    console.log(destinations[counter])
-    let mainArea = document.querySelector('.main')
-    mainArea.innerHTML = `<header class='welcome-message'><h2>Welcome to Travel Tracker</h2></header>
-    <section class='cycling-images'><img src="${destinations[counter].image}" alt="destination-image" class='cycling-images'>
-    <footer>Your Vacation Awaits!</footer></section>`
-    setTimeout(cycleImages, 3000, destinations, counter);
-}
-
-function logIn(event) {
-  debugger
-  const usernameArray = usernameInput.value.split('')
-  const usernameID = usernameArray.splice(8, 10).join('')
-  if(usernameInput.value === 'agency' && passwordInput.value === 'travel2020') {
-    displayAgencyDashboard()
-  } else if (usernameID <= 50 && passwordInput.value === 'travel2020') {
-    displayTravelerDashboard()
-  } else {
-    displayError()
-  }
-  event.preventDefault()
-}
-
-function displayAgencyDashboard() {
-  let mainArea = document.querySelector('.main')
-  mainArea.innerHTML = `<p>hey travel agent</p>`
-  usernameInput.value = ''
-  passwordInput.value = ''
-}
-
-function displayTravelerDashboard() {
-  let mainArea = document.querySelector('.main')
-  mainArea.innerHTML = `<p>hey traveler</p>`
-  usernameInput.value = ''
-  passwordInput.value = ''
-}
-
-function displayError() {
-  debugger
-  let form = document.getElementById('form1')
-  form.insertAdjacentHTML('afterend', `ERROR`)
-  usernameInput.value = ''
-  passwordInput.value = ''
-}
 
